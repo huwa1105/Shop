f9db4ecc4d8d4c5e82c9daa8645c5b801d98ddff
huwa1105 <hugo.walem@gmail.com>
2021-02-05 00:55:26 +0100

    Aprés installation du plugin PHP sur IntelliJ IDEA

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link href="css/shop.css" rel="stylesheet">
+</head>
+<body>
+    <p>Ceci est un test</p>
+</body>
+</html>
